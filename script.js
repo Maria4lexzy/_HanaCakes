2780b106e8df7e533b757c393dab05471c0499b6
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2021-09-14 02:23:09 +0200

    Added Blog, Forms for Cake Customization + Other Updates

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var icon = document.getElementsByClassName("animated-icon2");
-var sticky = document.getElementById("sticky-headeri");
+var sticky = document.getElementById("sticky-header");
+var quantityInput = document.getElementById("quantity-input");
+var accordionContainer = document.getElementById("accordion-container");
+const stickyWrapper = document.createElement("div");
 
+var stickyTop;
 
 window.onload = function () {
 
@@ -31,23 +35,41 @@ function toggleBtnEffect() {
 // Add the is-sticky class to the header when you reach its scroll position. 
 //Remove "is-sticky" when you leave the scroll position
 function stickyHeaderEffect() {
-  const stickyWrapper = document.createElement("div");
   var stickyHeight = sticky.offsetHeight;
 
 
   stickyWrapper.classList.add("sticky-wrapper");
   sticky.before(stickyWrapper);
   sticky.classList.add("sticky");
-  console.log(sticky);
+  stickyTop = stickyWrapper.offsetTop;
+  console.log(stickyTop);
 
 
-  if (window.pageYOffset >= 152) {
+  if (window.pageYOffset >= stickyTop) {
     stickyWrapper.style.height = stickyHeight;
-
     sticky.classList.add("is-sticky");
+    accordionContainer.classList.remove("container");
   } else {
     sticky.classList.remove("is-sticky");
+    accordionContainer.classList.add("container");
     stickyWrapper.style.height = 'auto';
+  }
+}
+// INCREMENT BUTTON
+function counter(type) {
+
+  if (type == "increase") {
+    quantityInput.value++;
+    console.log(quantityInput.value);
+
+  }
+  else if (type == 'decrease') {
+    if (quantityInput.value == 1) {
+      quantityInput.value = 1;
+    }
+    else {
+      quantityInput.value--;
+    }
   }
 }
 
